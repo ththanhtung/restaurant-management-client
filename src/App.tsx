a4202675f75856ca78795acee3311749738a9667
ththanhtung <nguyenththanhtung317@gmail.com>
2023-07-07 20:40:26 +0700

    refactor: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import { Layout, Public } from './components';
+import { GlobalStyles, Layout, Public } from './components';
 import { RequireAuth, Signup } from './features/auth';
 
 function App() {
   return (
     <>
+      <GlobalStyles/>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Public />}></Route>
